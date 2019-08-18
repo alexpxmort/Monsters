8a8144919d2eebf6798ee043f07cefc204814565
Alex Pereira de Oliveira <lexpdigi@gmail.com>
2019-08-18 12:31:54 -0300

    Updates

---
+++ precache-manifest.0cb8cb5667b4b499c7e3c95a3e6ed723.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30145c3f1a3f6c2cfe27b80af5520065",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "f0fe9ea70dfcb348fd6d",
+    "url": "/Monsters/static/css/main.1974a4ec.chunk.css"
+  },
+  {
+    "revision": "2ab373573a2da60aeefa",
+    "url": "/Monsters/static/js/2.b3f2c5c4.chunk.js"
+  },
+  {
+    "revision": "f0fe9ea70dfcb348fd6d",
+    "url": "/Monsters/static/js/main.6b9af08b.chunk.js"
+  },
+  {
+    "revision": "7e71cfb3eabbf75eeeff",
+    "url": "/Monsters/static/js/runtime~main.e1d3e109.js"
+  }
+]);
